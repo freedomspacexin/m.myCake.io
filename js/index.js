4b3ab1bc7139e28b0a1ac0d9b75c4996ae40bdd7
xin <freedomspace_xin@163.com>
2019-03-24 22:03:12 +0800

    添加好月圆文章

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 window.onload = function () {
-    new Swiper('.swiper-container',{
+    new Swiper('#swiper-container',{
         autoplay:5000,
         loop:true,
         pagination:'.swiper-pagination',
         autoplayDisableOnInteraction:false
+    });
+
+    new Swiper('#swiper-artricle', {
+        slidesPerView: 'auto',
+        spaceBetween: 10,
     });
 };
