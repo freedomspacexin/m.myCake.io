0c7d4397e7ce9f85f33de4ee3ccf7bbbdc09dfa8
xin <freedomspace_xin@163.com>
2019-03-25 22:03:07 +0800

    添加菜单显示，与删除公告

--- js/index.js
+++ js/index.js
@@ -12,9 +12,28 @@ window.onload = function () {
     });
 };
 
-$(function(){
-    loadLazy();
 
+
+$(function(){
+    var $menuIcon = $('.left-icon');
+    var $menu = $('.top-header-menu');
+    /*左上菜单*/
+    $menuIcon.on('tap',function(){
+        if ($menu.hasClass('show')){
+            $menu.removeClass('show');
+            $(this).children('i').removeClass('icomoon_font_close').addClass('icomoon_font_menu');
+        } else {
+            $menu.addClass('show');
+            $(this).children('i').removeClass('icomoon_font_menu').addClass('icomoon_font_close');
+        }
+    });
+    /*删除公告*/
+    var $closeNotice = $('.close-notice');
+    $closeNotice.on('tap', function(){
+        $(this).parent().hide();
+    });
+
+    loadLazy();
     function loadLazy(){
         var imgEles = $("img[data-original][lazyload]");
         $.each(imgEles,function(index,element){
@@ -46,19 +65,19 @@ $(function(){
         var elementWidth = element.offsetWidth;
         var onScreenHeight = ON_SCREEN_HEIGHT > elementHeight ? elementHeight : ON_SCREEN_HEIGHT;
         var onScreenWidth = ON_SCREEN_WIDTH > elementWidth ? elementWidth : ON_SCREEN_WIDTH;
-        // 元素在屏幕上方
+        // 元素在屏幕上?
         var elementBottomToWindowTop = rect.top + elementHeight;
         var bottomBoundingOnScreen = elementBottomToWindowTop >= onScreenHeight;
 
-        // 元素在屏幕下方
+        // 元素在屏幕下?
         var elementTopToWindowBottom = windowHeight - (rect.bottom - elementHeight);
         var topBoundingOnScreen = elementTopToWindowBottom >= onScreenHeight;
 
-        // 元素在屏幕左侧
+        // 元素在屏幕左?
         var elementRightToWindowLeft = rect.left + elementWidth;
         var rightBoundingOnScreen = elementRightToWindowLeft >= onScreenWidth;
 
-        // 元素在屏幕右侧
+        // 元素在屏幕右?
         var elementLeftToWindowRight = windowWidth - (rect.right - elementWidth);
         var leftBoundingOnScreen = elementLeftToWindowRight >= onScreenWidth;
 
