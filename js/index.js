bd59bf4e48737f2cbaf26bbdc372052bb1a69106
xin <freedomspace_xin@163.com>
2019-03-25 19:26:18 +0800

    添加图片懒加载功能

--- js/index.js
+++ js/index.js
@@ -11,3 +11,59 @@ window.onload = function () {
         spaceBetween: 10,
     });
 };
+
+$(function(){
+    loadLazy();
+
+    function loadLazy(){
+        var imgEles = $("img[data-original][lazyload]");
+        $.each(imgEles,function(index,element){
+            var $ele = $(element);
+            if ($ele.attr('data-original') == ""){
+                return;
+            }
+            if (isOnScreen($ele)){
+                var img = new Image();
+                img.src = $ele.attr("data-original");
+                img.onload = function() {
+                    $ele.attr('src', img.src);
+                }
+                $ele.removeAttr('data-original');
+                $ele.removeAttr('lazyload');
+            }
+        });
+    }
+    //元素是否在屏幕上
+    function isOnScreen(element) {
+        var ON_SCREEN_HEIGHT =50;
+        var ON_SCREEN_WIDTH = 50;
+        element = element.get(0);
+        var rect = element.getBoundingClientRect();
+        var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+        var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+
+        var elementHeight = element.offsetHeight;
+        var elementWidth = element.offsetWidth;
+        var onScreenHeight = ON_SCREEN_HEIGHT > elementHeight ? elementHeight : ON_SCREEN_HEIGHT;
+        var onScreenWidth = ON_SCREEN_WIDTH > elementWidth ? elementWidth : ON_SCREEN_WIDTH;
+        // 元素在屏幕上方
+        var elementBottomToWindowTop = rect.top + elementHeight;
+        var bottomBoundingOnScreen = elementBottomToWindowTop >= onScreenHeight;
+
+        // 元素在屏幕下方
+        var elementTopToWindowBottom = windowHeight - (rect.bottom - elementHeight);
+        var topBoundingOnScreen = elementTopToWindowBottom >= onScreenHeight;
+
+        // 元素在屏幕左侧
+        var elementRightToWindowLeft = rect.left + elementWidth;
+        var rightBoundingOnScreen = elementRightToWindowLeft >= onScreenWidth;
+
+        // 元素在屏幕右侧
+        var elementLeftToWindowRight = windowWidth - (rect.right - elementWidth);
+        var leftBoundingOnScreen = elementLeftToWindowRight >= onScreenWidth;
+
+        return bottomBoundingOnScreen && topBoundingOnScreen && rightBoundingOnScreen; /*&& leftBoundingOnScreen;*/
+    };
+
+     $(window).scroll(loadLazy);
+});
